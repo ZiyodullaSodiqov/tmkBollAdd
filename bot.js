6be5911f43e0e3064805260632244bdb1cfb80b8
Ziyodulla Sodiqov <107835398+ZiyodullaSodiqov@users.noreply.github.com>
2025-08-02 07:06:41 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ const bot = new TelegramBot(token, {
 
 
 const app = express();
-const port = process.env.PORT || 0.0.0.0;
+const port = process.env.PORT || 3000;
 
 app.get('/health', (req, res) => {
     res.status(200).json({ status: 'ok', message: 'Bot is running' });
